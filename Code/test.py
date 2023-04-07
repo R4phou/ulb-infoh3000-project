1639c74e23d32b334db0412f2c0b27d579cf4416
Exagor <alexandre.achten@gmail.com>
2023-04-07 14:19:20 +0200

    Test plot matrix

--- Code/test.py
+++ Code/test.py
@@ -1,6 +1,18 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-matrice = np.loadtxt("value.txt", delimiter=",")
+matrice = np.loadtxt("Results/sol_claq.txt")
+fig, ax = plt.subplots()
 
+min_val, max_val = 0, 70
+
+intersection_matrix = np.random.randint(0, 10, size=(max_val, max_val))
+
+ax.matshow(intersection_matrix, cmap=plt.cm.Blues)
+
+for i in range(15):
+    for j in range(15):
+        c = intersection_matrix[j,i]
+        ax.text(i, j, str(c), va='center', ha='center')
+plt.show()
 print(matrice)
