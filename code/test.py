c54ec8f073ec62e82c16d0e8467f807042493a7b
Humblet Raphaël <raphum0182@gmail.com>
2023-04-20 16:44:49 +0200

    manquait un petit truc

--- code/test.py
+++ code/test.py
@@ -16,26 +16,3 @@ mat = np.loadtxt("results/scores_gen300_pop500.csv",
 MAX_COMP = 5500
 MAX_PROD = 2.6
 MAX_PROX = 330
-
-# Diviser les points en coordonnées x, y et z
-x = mat[:, 0]
-y = mat[:, 1]
-z = mat[:, 2]
-x = normalize(x, MAX_PROD)
-y = normalize(y, MAX_PROX)
-z = normalize(z, MAX_COMP)
-
-
-# Créer une grille régulière pour la surface
-xi = np.linspace(min(x), max(x), 100)
-yi = np.linspace(min(y), max(y), 100)
-xi, yi = np.meshgrid(xi, yi)
-
-# Interpoler les valeurs de z sur la grille régulière à l'aide de la méthode de l'interpolation
-zi = griddata((x, y), z, (xi, yi), method='linear')
-
-# Visualiser la surface interpolée à l'aide de Matplotlib
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(xi, yi, zi)
-plt.show()
